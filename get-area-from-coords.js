2c180843fd3bda743a792d21a1178b7d5d994667
James Lave <30740613+jlave-dev@users.noreply.github.com>
2021-04-03 20:05:26 -0500

    Initial commit

---
+++ get-area-from-coords.js
@@ -0,0 +1,14 @@
+const GeoJsonGeometriesLookup = require("geojson-geometries-lookup");
+const geoJson = require("./chicago.json");
+
+const glookup = new GeoJsonGeometriesLookup(geoJson);
+
+module.exports = function getAreaFromCoords(lat, long) {
+  const point = { type: "Point", coordinates: [long, lat] };
+  const containers = glookup.getContainers(point);
+  if (containers.features && containers.features.length > 0) {
+    return containers.features[0];
+  } else {
+    return undefined;
+  }
+};
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const getAreaFromCoords = require("./get-area-from-coords");
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  const { lat, long } = req.query;
+  if (!lat || !long) {
+    res.status(404).send("lat or long query parameters were missing.");
+  }
+  const area = getAreaFromCoords(lat, long);
+  if (!area) {
+    res.status(404).send();
+  } else {
+    res.send(area);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
