08857d683b96ba8e7a711ad16ed8182a7280b31e
James Lave <30740613+jlave-dev@users.noreply.github.com>
2021-04-03 21:09:40 -0500

    Log server errors

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const getZipFromCoords = require("./get-zip-from-coords");
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  const { lat, long } = req.query;
-  if (!lat || !long) {
-    return res.status(404).send("lat or long query parameters were missing.");
+  try {
+    const { lat, long } = req.query;
+    if (!lat || !long) {
+      return res.status(404).send("lat or long query parameters were missing.");
+    }
+    const zip = getZipFromCoords(lat, long);
+    return res.send(zip);
+  } catch (error) {
+    console.error(error);
+    return res.status(500);
   }
-  const zip = getZipFromCoords(lat, long);
-  return res.send(zip);
 });
 
 app.listen(PORT, () => {
